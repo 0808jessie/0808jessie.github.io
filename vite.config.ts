524eb72a7b6250dfb2ded652006026c109e177c9
0808jessie <jessie3662943@gmail.com>
2026-07-12 08:42:44 +0800

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // 使用標準的 Vite 設定，這樣才能完美支援 base 路徑與 GitHub Pages 部署
 export default defineConfig({
-  base: "./", // 確保打包後的路徑為相對路徑，徹底解決白畫面問題
+  base: "/", // 確保打包後的路徑為相對路徑，徹底解決白畫面問題
   plugins: [
     react(),
     tsconfigPaths(), // 確保支援專案中的 @/ 路徑別名
