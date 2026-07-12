199ed18610ee5ca57255586e6192d869e516f736
0808jessie <jessie3662943@gmail.com>
2026-07-13 01:18:21 +0800

    bug網頁沒更新

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { DecideNow } from "./routes/index";
+import DecideNow from "./routes/index"; // 移除大括號，這樣就是讀取 default export
 import "./styles.css";
-
+import App from "./routes/index";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <DecideNow />
